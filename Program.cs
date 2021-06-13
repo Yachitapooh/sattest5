4fe9ee4f5bfc120838abaa2d3b7dbab00b115cef
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-06-13 17:19:42 +0700

    k5

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace sattestfinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ5
+            int item = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+            string type = Console.ReadLine();
+            string mode = Console.ReadLine();
+            while (mode == "ShowAll")
+            {
+                if (mode == "ShowAll")
+                {
+                    Console.WriteLine("{0}", name);
+                    Console.WriteLine("{0}", type);
+                    Console.WriteLine();
+                }
+                else if (mode == "item") 
+                {
+                    Console.WriteLine("{0}", name);
+                    Console.WriteLine("{0}", type);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("End");
+                }
+            }
+
+            Console.ReadLine(); 
+
+
+
+        }
+    }
+}
